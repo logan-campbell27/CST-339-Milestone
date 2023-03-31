27042929cb73b3ee56ac2ee15fd03bfd7d300888
Caden Silvey <caden@silveys.net>
2023-03-31 13:26:17 -0700

    Add Users to Database through Registration

--- src/main/java/com/gcu/RegistrationController.java
+++ src/main/java/com/gcu/RegistrationController.java
@@ -5,15 +5,15 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+// import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.gcu.business.RegistrationBusinessServiceInterface;
 import com.gcu.business.SecurityServiceInterface;
 import com.gcu.data.OrdersDataService;
 import com.gcu.data.RegistrationDataService;
@@ -21,7 +21,7 @@ import com.gcu.model.OrderModel;
 import com.gcu.model.LoginModel;
 import com.gcu.model.RegistrationModel;
 
-@Controller
+@RestController
 @RequestMapping("/register")
 public class RegistrationController{
 	
